c9a715f437338fc9dfc69297d6bb33c549607f71
Martin Vysny <mavi@vaadin.com>
2025-12-05 19:20:13 +0200

    QueryDP: initial impl

--- src/main/kotlin/com/vaadin/starter/beveragebuddy/backend/ktorm/QueryDataProvider.kt
+++ src/main/kotlin/com/vaadin/starter/beveragebuddy/backend/ktorm/QueryDataProvider.kt
@@ -1,30 +1,47 @@
 package com.vaadin.starter.beveragebuddy.backend.ktorm
 
-import com.vaadin.flow.data.binder.Binder
-import com.vaadin.flow.data.provider.*
+import com.vaadin.flow.data.provider.AbstractBackEndDataProvider
+import com.vaadin.flow.data.provider.ConfigurableFilterDataProvider
+import com.vaadin.flow.data.provider.DataProvider
 import com.vaadin.flow.data.provider.Query
+import com.vaadin.flow.data.provider.SortDirection
 import com.vaadin.flow.function.SerializableFunction
-import org.ktorm.dsl.*
-import org.ktorm.entity.Entity
+import org.ktorm.database.Database
+import org.ktorm.dsl.QueryRowSet
+import org.ktorm.dsl.QuerySource
+import org.ktorm.dsl.and
+import org.ktorm.dsl.asc
+import org.ktorm.dsl.desc
+import org.ktorm.dsl.from
+import org.ktorm.dsl.limit
+import org.ktorm.dsl.map
+import org.ktorm.dsl.offset
+import org.ktorm.dsl.orderBy
+import org.ktorm.dsl.select
+import org.ktorm.dsl.where
 import org.ktorm.entity.count
 import org.ktorm.entity.filter
 import org.ktorm.entity.sequenceOf
 import org.ktorm.expression.OrderByExpression
-import org.ktorm.schema.*
-import org.ktorm.support.postgresql.ilike
+import org.ktorm.schema.ColumnDeclaring
 import java.util.stream.Stream
-import kotlin.reflect.KProperty1
 
 /**
- * Loads entities [T] from given [table]. Example of use:
+ * Loads data from a ktorm [Query]. Mostly used for more complex stuff like joins; for selecting
+ * entities use [EntityDataProvider]. Example of use:
  * ```
- * val dp = Employees.dataProvider
- * TODO filter
+ * val dp = QueryDataProvider({ it
+ *   .from(Employees)
+ *   .leftJoin(Departments, on = Employees.departmentId eq Departments.id))
+ *   .select(Employees.name, Departments.name)
+ * }, { row -> Names(row[Employees.name], row[Departments.name]) })
+ * TODO dp.filter
  * ```
- * @param table the table to load the entities from
- * @param T entity type
+ * @param query creates [org.ktorm.dsl.Query]
+ * @param rowMapper converts [QueryRowSet] to the bean [T]
+ * @param T the bean type returned by this data provider.
  */
-class EntityDataProvider<T: Entity<T>>(val table: Table<T>) : AbstractBackEndDataProvider<T, ColumnDeclaring<Boolean>>(),
+class QueryDataProvider<T>(val query: (Database) -> org.ktorm.dsl.Query, val rowMapper: (QueryRowSet) -> T) : AbstractBackEndDataProvider<T, ColumnDeclaring<Boolean>>(),
     ConfigurableFilterDataProvider<T, ColumnDeclaring<Boolean>, ColumnDeclaring<Boolean>> {
 
     private var filter: ColumnDeclaring<Boolean>? = null
@@ -83,51 +100,4 @@ class EntityDataProvider<T: Entity<T>>(val table: Table<T>) : AbstractBackEndDat
             ) else null
         }
     }
-}
-
-/**
- * Returns Vaadin {@link EntityDataProvider} which loads instances of this entity. See {@link EntityDataProvider}
- * for more information.
- */
-val <E: Entity<E>> Table<E>.dataProvider: EntityDataProvider<E> get() = EntityDataProvider(this)
-
-/**
- * Returns a [DataProvider] which accepts a [String] filter; when a non-blank String is provided,
- * a `col ilike string%` where clause is added to the query.
- * Example of use:
- * ```
- * setItems(Categories.dataProvider.withStringFilterOn(Categories.name))
- * ```
- * @param column the [Table] column from entity [T]
- * @param T the entity type
- * @return [DataProvider] which matches filter string against the value of given [column].
- */
-fun <T: Entity<T>> EntityDataProvider<T>.withStringFilterOn(column: Column<String>): DataProvider<T, String> =
-    withStringFilter {
-        column.ilike("${it.trim()}%")
-    }
-
-/**
- * A type-safe binding which binds only to a property of given type, found on given bean.
- * @param column the ktorm column
- */
-fun <BEAN, FIELDVALUE:Any> Binder.BindingBuilder<BEAN, FIELDVALUE?>.bind(column: Column<out FIELDVALUE>): Binder.Binding<BEAN, FIELDVALUE?> {
-    val binding: KProperty1<*, *>
-    if (column.binding is ReferenceBinding) {
-        binding = (column.binding as ReferenceBinding).onProperty
-    } else {
-        val properties = (column.binding as NestedBinding).properties
-        require(properties.size == 1) { "$column: nested properties aren't supported: ${column.binding}" }
-        binding = properties[0]
-    }
-
-    // oh crap, don't use binding by getter and setter - validations won't work!
-    // we need to use bind(String) even though that will use undebuggable crappy Java 8 lambdas :-(
-    //        bind({ bean -> prop.get(bean) }, { bean, value -> prop.set(bean, value) })
-    var name = binding.name
-    if (name.startsWith("is")) {
-        // Kotlin KProperties named "isFoo" are represented with just "foo" in the bean property set
-        name = name[2].lowercase() + name.drop(3)
-    }
-    return bind(name)
 }
