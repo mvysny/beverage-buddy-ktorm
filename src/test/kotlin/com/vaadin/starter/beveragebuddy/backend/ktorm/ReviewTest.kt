fb0808a69ac5822441bd404bd77dd22c95f0d673
Martin Vysny <mavi@vaadin.com>
2025-12-05 18:50:43 +0200

    Add ReviewTest

---
+++ src/test/kotlin/com/vaadin/starter/beveragebuddy/backend/ktorm/ReviewTest.kt
@@ -0,0 +1,30 @@
+package com.vaadin.starter.beveragebuddy.backend.ktorm
+
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import java.time.LocalDate
+import kotlin.test.expect
+
+class ReviewTest {
+    @Nested
+    inner class validation() {
+        @Test
+        fun smoke() {
+            expect(true) {
+                Review {
+                    name = "Foo"; category = 1L; score = 1; count = 1; date =
+                    LocalDate.now()
+                }.isValid
+            }
+            expect(false) { Review {}.isValid }
+            expect(false) { Review { category = 1L }.isValid }
+            expect(false) { Review { name = "Foo" }.isValid }
+            expect(false) { Review { name = "F"; category = 1L }.isValid }
+            expect(false) {
+                Review {
+                    score = 10; name = "Foo"; category = 1L
+                }.isValid
+            }
+        }
+    }
+}
