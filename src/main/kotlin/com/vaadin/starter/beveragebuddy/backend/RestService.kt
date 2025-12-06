30167e156268cd079d835416af39af88245cd221
Martin Vysny <mavi@vaadin.com>
2025-12-06 08:14:20 +0200

    RestServiceTest partly ported to ktorm

--- src/main/kotlin/com/vaadin/starter/beveragebuddy/backend/RestService.kt
+++ src/main/kotlin/com/vaadin/starter/beveragebuddy/backend/RestService.kt
@@ -1,11 +1,15 @@
 package com.vaadin.starter.beveragebuddy.backend
 
+import com.vaadin.starter.beveragebuddy.backend.ktorm.Categories
+import com.vaadin.starter.beveragebuddy.backend.ktorm.Category
+import com.vaadin.starter.beveragebuddy.backend.ktorm.findAll
 import eu.vaadinonkotlin.rest.*
 import io.javalin.Javalin
 import jakarta.servlet.annotation.WebServlet
 import jakarta.servlet.http.HttpServlet
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import java.time.LocalDate
 
 /**
  * Provides access to person list. To test, just run `curl http://localhost:8080/rest/categories`
@@ -13,7 +17,7 @@ import jakarta.servlet.http.HttpServletResponse
 @WebServlet(urlPatterns = ["/rest/*"], name = "JavalinRestServlet", asyncSupported = false)
 class JavalinRestServlet : HttpServlet() {
     val javalin = Javalin.createStandalone { it.gsonMapper(VokRest.gson) } .apply {
-        get("/rest/categories") { ctx -> ctx.json(Category.findAll()) }
+        get("/rest/categories") { ctx -> ctx.json(Categories.findAll().map { RestCategory.of(it) }) }
         crud2("/rest/reviews", Review.getCrudHandler(false))
     }.javalinServlet()
 
@@ -21,3 +25,21 @@ class JavalinRestServlet : HttpServlet() {
         javalin.service(req, resp)
     }
 }
+
+data class RestCategory(
+    var id: Long? = null,
+    var name: String = ""
+) {
+    companion object {
+        fun of(cat: Category): RestCategory = RestCategory(
+            cat.id, cat.name
+        )
+    }
+}
+
+data class RestReview(var id: Long? = null,
+                      var score: Int = 1,
+                      var name: String = "",
+                      var date: LocalDate = LocalDate.now(),
+                      var category: Long? = null,
+                      var count: Int = 1)
--- src/test/kotlin/com/vaadin/starter/beveragebuddy/backend/RestServiceTest.kt
+++ src/test/kotlin/com/vaadin/starter/beveragebuddy/backend/RestServiceTest.kt
@@ -1,13 +1,22 @@
 package com.vaadin.starter.beveragebuddy.backend
 
 import com.github.mvysny.kaributesting.v10.expectList
+import com.github.vokorm.KEntity
 import com.vaadin.starter.beveragebuddy.AbstractAppTest
+import com.vaadin.starter.beveragebuddy.backend.ktorm.Category
 import eu.vaadinonkotlin.restclient.*
+import jakarta.validation.constraints.Max
+import jakarta.validation.constraints.Min
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.PastOrPresent
+import jakarta.validation.constraints.Size
 import org.eclipse.jetty.ee10.webapp.WebAppContext
 import org.eclipse.jetty.server.Server
 import org.junit.jupiter.api.*
 import java.io.FileNotFoundException
 import java.net.http.HttpClient
+import java.time.LocalDate
 
 /**
  * Uses the VoK `vok-rest-client` module for help with testing of the REST endpoints. See docs on the
@@ -18,13 +27,13 @@ class PersonRestClient(val baseUrl: String) {
         require(!baseUrl.endsWith("/")) { "$baseUrl must not end with a slash" }
     }
     private val client: HttpClient = VokRestClient.httpClient
-    fun getAllCategories(): List<Category> {
+    fun getAllCategories(): List<RestCategory> {
         val request = "$baseUrl/categories".buildUrl().buildRequest()
-        return client.exec(request) { response -> response.jsonArray(Category::class.java) }
+        return client.exec(request) { response -> response.jsonArray(RestCategory::class.java) }
     }
-    fun getAllReviews(): List<Review> {
+    fun getAllReviews(): List<RestReview> {
         val request = "$baseUrl/reviews".buildUrl().buildRequest()
-        return client.exec(request) { response -> response.jsonArray(Review::class.java) }
+        return client.exec(request) { response -> response.jsonArray(RestReview::class.java) }
     }
     fun nonexistingEndpoint() {
         val request = "$baseUrl/nonexisting".buildUrl().buildRequest()
@@ -56,6 +65,11 @@ class RestServiceTest : AbstractAppTest() {
 
     @Test fun `categories smoke test`() {
         expectList() { client.getAllCategories() }
+    }
+
+    @Test fun `categories retrieval`() {
+        val cat = Category { name = "foo" } .apply { save() }
+        expectList(RestCategory.of(cat)) { client.getAllCategories() }
     }
 
     @Test fun `reviews smoke test`() {
