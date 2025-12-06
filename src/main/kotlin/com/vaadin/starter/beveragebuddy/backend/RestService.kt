e4962bd32d9bff17c4115896cd2b4b21a78ac0a2
Martin Vysny <mavi@vaadin.com>
2025-12-06 08:19:22 +0200

    port RestService to ktorm

--- src/main/kotlin/com/vaadin/starter/beveragebuddy/backend/RestService.kt
+++ src/main/kotlin/com/vaadin/starter/beveragebuddy/backend/RestService.kt
@@ -2,6 +2,8 @@ package com.vaadin.starter.beveragebuddy.backend
 
 import com.vaadin.starter.beveragebuddy.backend.ktorm.Categories
 import com.vaadin.starter.beveragebuddy.backend.ktorm.Category
+import com.vaadin.starter.beveragebuddy.backend.ktorm.Reviews
+import com.vaadin.starter.beveragebuddy.backend.ktorm.Review
 import com.vaadin.starter.beveragebuddy.backend.ktorm.findAll
 import eu.vaadinonkotlin.rest.*
 import io.javalin.Javalin
@@ -18,7 +20,7 @@ import java.time.LocalDate
 class JavalinRestServlet : HttpServlet() {
     val javalin = Javalin.createStandalone { it.gsonMapper(VokRest.gson) } .apply {
         get("/rest/categories") { ctx -> ctx.json(Categories.findAll().map { RestCategory.of(it) }) }
-        crud2("/rest/reviews", Review.getCrudHandler(false))
+        get("/rest/reviews") { ctx -> ctx.json(Reviews.findAll().map { RestReview.of(it) }) }
     }.javalinServlet()
 
     override fun service(req: HttpServletRequest, resp: HttpServletResponse) {
@@ -40,6 +42,12 @@ data class RestCategory(
 data class RestReview(var id: Long? = null,
                       var score: Int = 1,
                       var name: String = "",
-                      var date: LocalDate = LocalDate.now(),
+                      var date: LocalDate? = null,
                       var category: Long? = null,
-                      var count: Int = 1)
+                      var count: Int = 1) {
+    companion object {
+        fun of(review: Review) = RestReview(
+            review.id, review.score, review.name, review.date, review.category, review.count
+        )
+    }
+}
